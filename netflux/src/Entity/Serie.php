b83ef6fea932873967eb0c7d822e3567cbe2fa14
Thibaud Vepierre <thibaud@MacBook-Pro-de-Thibaud.local>
2019-07-11 21:40:22 +0200

    Setup BDD

---
+++ netflux/src/Entity/Serie.php
@@ -0,0 +1,143 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\SerieRepository")
+ */
+class Serie
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nom;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $datestart;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $dateend;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $nbsaison;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $affiche;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\categorie")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $categorie;
+
+    /**
+     * @ORM\Column(type="string", length=500)
+     */
+    private $synopsis;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getDatestart(): ?int
+    {
+        return $this->datestart;
+    }
+
+    public function setDatestart(int $datestart): self
+    {
+        $this->datestart = $datestart;
+
+        return $this;
+    }
+
+    public function getDateend(): ?int
+    {
+        return $this->dateend;
+    }
+
+    public function setDateend(int $dateend): self
+    {
+        $this->dateend = $dateend;
+
+        return $this;
+    }
+
+    public function getNbsaison(): ?int
+    {
+        return $this->nbsaison;
+    }
+
+    public function setNbsaison(int $nbsaison): self
+    {
+        $this->nbsaison = $nbsaison;
+
+        return $this;
+    }
+
+    public function getAffiche(): ?string
+    {
+        return $this->affiche;
+    }
+
+    public function setAffiche(string $affiche): self
+    {
+        $this->affiche = $affiche;
+
+        return $this;
+    }
+
+    public function getCategorie(): ?categorie
+    {
+        return $this->categorie;
+    }
+
+    public function setCategorie(?categorie $categorie): self
+    {
+        $this->categorie = $categorie;
+
+        return $this;
+    }
+
+    public function getSynopsis(): ?string
+    {
+        return $this->synopsis;
+    }
+
+    public function setSynopsis(string $synopsis): self
+    {
+        $this->synopsis = $synopsis;
+
+        return $this;
+    }
+}
